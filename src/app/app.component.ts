ba0b92b336987830f4d8d821033d706d55a805a9
akash kale <kale64136@gmail.com>
2025-05-23 22:52:32 +0530

    work on tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,34 @@ ngOnInit(): void {
     console.log(signUpForm);
     
   }
+
+
+skillsArray : Array<any>= [
+  {
+    skillname :'sass',
+    skillInfo :`<strong>sass</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
+`
+  },
+  {
+    skillname :'typescript',
+    skillInfo :`<strong>typescript</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
+`
+  },
+  {
+    skillname :'bootstrap',
+    skillInfo :`<strong>bootstrap</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
+`
+  },
+  {
+    skillname :'rxjs',
+    skillInfo :`<strong>rxjs</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
+`
+  }
+ ]
+
+ selectedskill = this.skillsArray[2].skillname;
+  constructor() { }
+
+
+
 }
